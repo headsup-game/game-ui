5954e104d735480847d7470d0a2a2e99ed9bf93d
Pratham <chudasamapratham142@gmail.com>
2024-12-06 21:56:21 +0530

    removed the usermoadl from her to the parent

--- src/app/game/RecentBets.tsx
+++ src/app/game/RecentBets.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useState } from "react";
-import { ConfigProvider, Flex, Table, Typography } from "antd";
+import { Button, ConfigProvider, Flex, Table, Typography } from "antd";
 import Container from "app/components/Container/Container";
 import styles from "./Game.module.scss";
 import { gql, useQuery } from "@apollo/client";
@@ -12,95 +12,113 @@ import CardSet from "@components/CardSet";
 import { Card } from "interfaces/card";
 import { Pagination } from "antd";
 import { useAccount } from "wagmi";
-import { ethers } from 'ethers'
+import { ethers } from "ethers";
 import { AlignType } from "rc-table/lib/interface";
 
 const { Title } = Typography;
 
-const RecentBets = React.memo(() => {
+const RecentBets = React.memo(({
+	setShowUserBetModal
+} : {
+	setShowUserBetModal: React.Dispatch<React.SetStateAction<boolean>>
+}) => {
   // State for table columns and data source
   const [columns, setColumns] = useState([
     {
       title: "Round#",
       width: "5",
-      align: 'center' as AlignType,
+      align: "center" as AlignType,
       dataIndex: "roundNumber",
       key: "roundNumber",
       render: (roundNumber?: string) =>
-        roundNumber ? (
-          <span>{roundNumber}</span>
-        ) : (
-          <span>0</span>
-        ),
-    }, {
+        roundNumber ? <span>{roundNumber}</span> : <span>0</span>,
+    },
+    {
       title: "Apes",
-      align: 'center' as AlignType,
+      align: "center" as AlignType,
       dataIndex: "apeCards",
       key: "apeCards",
-      render: (data: { cards: Card[], bets: string }) =>
+      render: (data: { cards: Card[]; bets: string }) =>
         Array.isArray(data.cards) ? (
-          <><CardSet isSmall={true} numberOfCards={data.cards.length} cards={data.cards} cardWidth={50} /></>
+          <>
+            <CardSet
+              isSmall={true}
+              numberOfCards={data.cards.length}
+              cards={data.cards}
+              cardWidth={50}
+            />
+          </>
         ) : (
           <span>No Cards</span>
         ),
     },
     {
       title: "Apes Pool",
-      align: 'center' as AlignType,
+      align: "center" as AlignType,
       dataIndex: "totalApesBets",
       key: "totalApesBets",
       render: (totalBets?: string) =>
         totalBets ? (
-          <span style={{ textAlign: 'right' }}>{totalBets}</span>
+          <span style={{ textAlign: "right" }}>{totalBets}</span>
         ) : (
-          <span style={{ textAlign: 'right' }}>0.0</span>
+          <span style={{ textAlign: "right" }}>0.0</span>
         ),
-    }, {
+    },
+    {
       title: "Punks",
       dataIndex: "punkCards",
-      align: 'center' as AlignType,
+      align: "center" as AlignType,
       key: "punkCards",
-      render: (data: { cards: Card[], bets: string }) =>
+      render: (data: { cards: Card[]; bets: string }) =>
         Array.isArray(data.cards) ? (
-          <><CardSet isSmall={true} numberOfCards={data.cards.length} cards={data.cards} cardWidth={50} /></>
+          <>
+            <CardSet
+              isSmall={true}
+              numberOfCards={data.cards.length}
+              cards={data.cards}
+              cardWidth={50}
+            />
+          </>
         ) : (
           <span>No Cards</span>
         ),
     },
     {
       title: "Punk Pool",
-      align: 'center' as AlignType,
+      align: "center" as AlignType,
       dataIndex: "totalPunkBets",
       key: "totalPunkBets",
       render: (totalBets?: string) =>
-        totalBets ? (
-          <span>{totalBets}</span>
-        ) : (
-          <span>0.0</span>
-        ),
+        totalBets ? <span>{totalBets}</span> : <span>0.0</span>,
     },
     {
       title: "Community Cards",
-      align: 'center' as AlignType,
+      align: "center" as AlignType,
       dataIndex: "communityCards",
       key: "communityCards",
       render: (cards: Card[]) =>
         Array.isArray(cards) ? (
-          <CardSet isSmall={true} numberOfCards={cards.length} cards={cards} cardWidth={50} />
+          <CardSet
+            isSmall={true}
+            numberOfCards={cards.length}
+            cards={cards}
+            cardWidth={50}
+          />
         ) : (
           <span>No Cards</span>
         ),
     },
     {
       title: "Winner",
-      align: 'center' as AlignType,
+      align: "center" as AlignType,
       dataIndex: "winner",
       key: "winner",
-      render: (winner: string) => (<span>{winner}</span>)
-    }, {
+      render: (winner: string) => <span>{winner}</span>,
+    },
+    {
       title: "Own Bet",
       dataIndex: "ownBets",
-      align: 'center' as AlignType,
+      align: "center" as AlignType,
       key: "ownBets",
       render: (bet: { amount: string; position: string; isWinner: boolean }) =>
         bet ? (
@@ -110,15 +128,14 @@ const RecentBets = React.memo(() => {
         ) : (
           <span>No Bets</span>
         ),
-    }, {
+    },
+    {
       title: "Own P/L",
       dataIndex: "ownWonAmount",
-      align: 'center' as AlignType,
+      align: "center" as AlignType,
       width: "10%",
       key: "ownWonAmount",
-      render: (ownWonAmount: string) => (
-        <span>{ownWonAmount}</span>
-      )
+      render: (ownWonAmount: string) => <span>{ownWonAmount}</span>,
     },
   ]);
 
@@ -126,14 +143,14 @@ const RecentBets = React.memo(() => {
     {
       title: "#",
       width: "15%",
-      align: 'center' as AlignType,
+      align: "center" as AlignType,
       dataIndex: "roundNumber",
       key: "roundNumber",
       render: (roundNumber?: string) => roundNumber || "0",
     },
     {
       title: "Game",
-      align: 'center' as AlignType,
+      align: "center" as AlignType,
       width: "55%",
       key: "gameInfo",
       render: (_: any, record: any) => (
@@ -142,14 +159,19 @@ const RecentBets = React.memo(() => {
             <small>Apes: {record.totalApesBets}</small>
             <small>Punks: {record.totalPunkBets}</small>
           </Flex>
-          <CardSet isSmall={true} numberOfCards={record.communityCards.length} cards={record.communityCards} cardWidth={30} />
+          <CardSet
+            isSmall={true}
+            numberOfCards={record.communityCards.length}
+            cards={record.communityCards}
+            cardWidth={30}
+          />
           <small>Winner: {record.winner}</small>
         </Flex>
       ),
     },
     {
       title: "Your Bet",
-      align: 'center' as AlignType,
+      align: "center" as AlignType,
       width: "30%",
       key: "betInfo",
       render: (_: any, record: any) => (
@@ -175,26 +197,32 @@ const RecentBets = React.memo(() => {
       roundNumber: string;
       key: string;
       communityCards: Card[];
-      punkCards: { cards: Card[], bets: string };
-      apeCards: { cards: Card[], bets: string };
+      punkCards: { cards: Card[]; bets: string };
+      apeCards: { cards: Card[]; bets: string };
       // amounts: { totalPunksBets: string; totalApesBets: string };
-      ownBets: { amount: string; position: string; isWinner: boolean } | null;
+      ownBets: {
+        amount: string;
+        position: string;
+        isWinner: boolean;
+      } | null;
       totalPunkBets: string;
       totalApesBets: string;
       winner: string;
       ownWonAmount: string;
     }[]
   >([]);
-  const [mobileDataSource, setMobileDataSource] = useState<{
-    roundNumber: string;
-    key: string;
-    communityCards: Card[];
-    totalApesBets: string;
-    totalPunkBets: string;
-    winner: string;
-    ownBets: { amount: string; position: string } | null;
-    ownWonAmount: string;
-  }[]>([]);
+  const [mobileDataSource, setMobileDataSource] = useState<
+    {
+      roundNumber: string;
+      key: string;
+      communityCards: Card[];
+      totalApesBets: string;
+      totalPunkBets: string;
+      winner: string;
+      ownBets: { amount: string; position: string } | null;
+      ownWonAmount: string;
+    }[]
+  >([]);
 
   // Function to handle data from the query
   const handleRoundData = (data: { rounds: RoundPage }) => {
@@ -202,18 +230,26 @@ const RecentBets = React.memo(() => {
       roundNumber: string;
       key: string;
       communityCards: Card[];
-      punkCards: { cards: Card[], bets: string };
-      apeCards: { cards: Card[], bets: string };
-      ownBets: { amount: string; position: string; isWinner: boolean } | null;
-      totalPunkBets: string,
-      totalApesBets: string
+      punkCards: { cards: Card[]; bets: string };
+      apeCards: { cards: Card[]; bets: string };
+      ownBets: {
+        amount: string;
+        position: string;
+        isWinner: boolean;
+      } | null;
+      totalPunkBets: string;
+      totalApesBets: string;
       winner: string;
       ownWonAmount: string;
     }[] = [];
 
     for (const round of data.rounds.items) {
       try {
-        const gameState: GameState = getGameStateFromRound(null, round, address);
+        const gameState: GameState = getGameStateFromRound(
+          null,
+          round,
+          address
+        );
 
         if (!gameState) {
           return;
@@ -224,12 +260,19 @@ const RecentBets = React.memo(() => {
         const totalApesBets = gameState.apesData.totalBetAmounts || "0.0";
 
         const communityCards = gameState.communityCards || [];
-        const apeCards = { cards: gameState.apesData.cards || [], bets: totalApesBets || "0.0." };
-        const punkCards = { cards: gameState.punksData.cards || [], bets: totalPunksBets || "0.0." };
-
+        const apeCards = {
+          cards: gameState.apesData.cards || [],
+          bets: totalApesBets || "0.0.",
+        };
+        const punkCards = {
+          cards: gameState.punksData.cards || [],
+          bets: totalPunksBets || "0.0.",
+        };
 
         // Extract user's own bet information
-        const ownBet = round.participants?.items.find((bet) => bet.userId == address) || null;
+        const ownBet =
+          round.participants?.items.find((bet) => bet.userId == address) ||
+          null;
 
         dataSource.push({
           winner: gameState.roundWinnerMessageShort,
@@ -243,12 +286,12 @@ const RecentBets = React.memo(() => {
           // amounts: { totalPunksBets, totalApesBets },
           ownBets: ownBet
             ? {
-              amount: ethers.formatEther(ownBet.amount),
-              position: ownBet.position,
-              isWinner: ownBet.position == round.winner,
-            }
+                amount: ethers.formatEther(ownBet.amount),
+                position: ownBet.position,
+                isWinner: ownBet.position == round.winner,
+              }
             : null,
-          ownWonAmount: "0.0"
+          ownWonAmount: "0.0",
         });
       } catch (error) {
         console.error("Error handling round data:", error);
@@ -270,11 +313,17 @@ const RecentBets = React.memo(() => {
 
     for (const round of data.rounds.items) {
       try {
-        const gameState: GameState = getGameStateFromRound(null, round, address);
+        const gameState: GameState = getGameStateFromRound(
+          null,
+          round,
+          address
+        );
         if (!gameState) continue;
 
         // Get user's bet information
-        const ownBet = round.participants?.items.find((bet) => bet.userId == address) || null;
+        const ownBet =
+          round.participants?.items.find((bet) => bet.userId == address) ||
+          null;
 
         // Add mobile data
         mobileDataSource.push({
@@ -284,22 +333,23 @@ const RecentBets = React.memo(() => {
           totalApesBets: gameState.apesData.totalBetAmounts || "0.0",
           totalPunkBets: gameState.punksData.totalBetAmounts || "0.0",
           winner: gameState.roundWinnerMessageShort,
-          ownBets: ownBet ? {
-            amount: ethers.formatEther(ownBet.amount),
-            position: ownBet.position
-          } : null,
-          ownWonAmount: "0.0"
+          ownBets: ownBet
+            ? {
+                amount: ethers.formatEther(ownBet.amount),
+                position: ownBet.position,
+              }
+            : null,
+          ownWonAmount: "0.0",
         });
       } catch (error) {
         console.error("Error handling round data:", error);
       }
     }
-    
+
     setMobileDataSource(mobileDataSource);
   };
 
   const [whereQuery, setWhereQuery] = useState<{ [key: string]: string }>({});
-
 
   // Apollo query to fetch round data
   useQuery<{ rounds: RoundPage }>(GET_CURRENT_ROUND_QUERY, {
@@ -309,85 +359,106 @@ const RecentBets = React.memo(() => {
     notifyOnNetworkStatusChange: true,
   });
 
-  const totalItems = useQuery<{ rounds: RoundPage }>
-                        (gql`query GetTotalRounds {
-                                rounds(orderBy: "epoch", orderDirection: "desc", limit: 1) {
-                                  items {
-                                    epoch
-                                  }
-                                }
-                              }`, { pollInterval: 12000 }).data?.rounds.items[0].epoch || 0;
+  const totalItems =
+    useQuery<{ rounds: RoundPage }>(
+      gql`
+        query GetTotalRounds {
+          rounds(orderBy: "epoch", orderDirection: "desc", limit: 1) {
+            items {
+              epoch
+            }
+          }
+        }
+      `,
+      { pollInterval: 12000 }
+    ).data?.rounds.items[0].epoch || 0;
   const [currentPage, setCurrentPage] = useState(1);
 
   function handlePageChange(page: number) {
     const itemsPerPage = 10;
-    const startEpoch = totalItems - ((page - 1) * itemsPerPage);
+    const startEpoch = totalItems - (page - 1) * itemsPerPage;
     const endEpoch = Math.max(startEpoch - itemsPerPage, 1);
 
     setWhereQuery({
       epoch_lte: String(startEpoch + 1),
-      epoch_gte: String(endEpoch)
+      epoch_gte: String(endEpoch),
     });
 
     setCurrentPage(page);
   }
 
   return (
-    <Container>
-      <Flex justify="space-between" align="flex-start">
-        <Title level={5} className={styles.RecentBetsTitle}>
-          Recent Rounds
-        </Title>
-        <Flex gap={8}>
-          <></>
-          {/* <Button 
-            type={activeQueryIsUserWinnings ? "primary" : "default"} 
-            size="small" 
-            onClick={() => handleRoundDataQuery(activeQueryIsUserWinnings ? "userWinnings" : "all")}
-          >
-            My Winnigs
-          </Button>
-          <Button 
-            type={activeQueryIsUnclaimed ? "primary" : "default"} 
-            size="small"
-            onClick={ () => handleRoundDataQuery(activeQueryIsUnclaimed ? "unclaimed" : "all") }
-          >
-            Unclaimed
-          </Button> */}
+    <>
+      <Container>
+        <Flex
+          justify="space-between"
+          align="flex-start"
+          style={{ marginTop: "32px" }}
+        >
+          <Title level={5} className={styles.RecentBetsTitle}>
+            Recent Rounds
+          </Title>
+          <Flex gap={8}>
+            <></>
+            <Button
+              type={"primary"}
+              size="small"
+              onClick={() => setShowUserBetModal(true)}
+            >
+              My Bets
+            </Button>
+          </Flex>
         </Flex>
-      </Flex>
-      <ConfigProvider
-        theme={{
-          components: {
-            Table: {
-              borderColor: "#a8a7af33",
-              headerBg: "#141127",
-              headerColor: "#6C6C89",
-              borderRadius: 8,
-              bodySortBg: "#141127",
-              colorBgContainer: "#141127",
-              colorText: "#F9FAFB",
+
+        <ConfigProvider
+          theme={{
+            components: {
+              Table: {
+                borderColor: "#a8a7af33",
+                headerBg: "#141127",
+                headerColor: "#6C6C89",
+                borderRadius: 8,
+                bodySortBg: "#141127",
+                colorBgContainer: "#141127",
+                colorText: "#F9FAFB",
+              },
+              Pagination: {
+                itemBg: "#141127",
+                colorIcon: "#F9FAFB",
+                colorText: "#F9FAFB",
+              },
             },
-            Pagination: {
-              itemBg: "#141127",
-              colorIcon: "#F9FAFB",
-              colorText: "#F9FAFB",
-            },
-          },
-        }}
-      >
-        <div className="hidden lg:block">
-          <Table dataSource={dataSource} columns={columns} pagination={false} />
-        </div>
-        <div className="lg:hidden">
-          <Table dataSource={mobileDataSource} columns={mobileColumns} pagination={false} />
-        </div>
-        <Pagination align="center" defaultCurrent={1} current={currentPage} total={totalItems} onChange={handlePageChange} style={{margin: "16px 0"}} />
-      </ConfigProvider>
-    </Container>
+          }}
+        >
+          <div className="hidden lg:block">
+            <Table
+              dataSource={dataSource}
+              columns={columns}
+              pagination={false}
+            />
+          </div>
+          <div className="lg:hidden">
+            <Table
+              dataSource={mobileDataSource}
+              columns={mobileColumns}
+              pagination={false}
+            />
+          </div>
+          <Pagination
+            align="center"
+            defaultCurrent={1}
+            current={currentPage}
+            total={totalItems}
+            onChange={handlePageChange}
+            style={{ margin: "16px 0" }}
+          />
+        </ConfigProvider>
+      </Container>
+      
+    </>
   );
 });
 
-RecentBets.displayName = 'RecentBets';
+RecentBets.displayName = "RecentBets";
 
 export default RecentBets;
